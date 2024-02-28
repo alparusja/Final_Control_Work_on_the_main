9836db24f9a6a1434da54ef1e49a5d5b9dc5458e
alparusja <alparusja@mail.ru>
2024-02-28 13:39:01 +0300

    Программа на  C# с 2мя методами

--- Задача/Program.cs
+++ Задача/Program.cs
@@ -6,3 +6,46 @@
 // При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter strings separated by commas:");
+        string input = Console.ReadLine()!;
+        string[] arr = input.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        string[] newArr = GetShortStrings(arr);
+
+        Console.WriteLine("New array of strings:");
+        foreach (var item in newArr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    static string[] GetShortStrings(string[] arr)
+    {
+        int count = 0;
+        foreach (var item in arr)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArr = new string[count];
+        int index = 0;
+        foreach (var item in arr)
+        {
+            if (item.Length <= 3)
+            {
+                newArr[index++] = item;
+            }
+        }
+
+        return newArr;
+    }
+}
